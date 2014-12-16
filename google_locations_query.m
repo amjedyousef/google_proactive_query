01ed7a378f8ad6223c3a0471fcac8bf8b2a06ad0
amjed <com_amjed@yahoo.com>
2014-12-16 09:52:10 +0100

    The code has been modified and now it is working and the results have been submitted

--- google_locations_query.m
+++ google_locations_query.m
@@ -1,5 +1,5 @@
 tic; 
-clear all; 
+clear; 
 close all; 
 clc;
 %%
@@ -7,35 +7,29 @@ clc;
 my_path='/home/amjed/Documents/Gproject/workspace/data/WSDB_DATA';
 %%
 %Global Google parameters (refer to https://developers.google.com/spectrum/v1/paws/getSpectrum)
-type='"AVAIL_SPECTRUM_REQ"';
 height= 30.0; %In meters; Note: 'height' needs decimal value
-agl='"AMSL"';
-num_of_steps = [1 2];% 2 4 8 16 32 64 128 256]; % will be increased inside the for loop
+num_of_steps = [1 2 4 8 16 32]% 64 128] %256]; 
+distance_divider =  num_of_steps(length(num_of_steps));
 key_counter = 0;
 fileId = 0 ;
-num_of_query_per_location = 1
+num_of_query_per_location = 13;
 %%
-
     %The data stored in the file as longitude latitude longitude latitude
     format long;
     long_lat = load('long_lat.txt');
     [r ,c] = size(long_lat);
-
+    delay_google_vec = [];
 for k=1:r    
         %Location data    
-        long_start= long_lat(k , 1);  
-        lat_start=long_lat(k , 2); 
-
-        long_end=long_lat(k , 3); 
-        lat_end=long_lat(k , 4); 
+        long_start= long_lat(k , 1)
+        lat_start=long_lat(k , 2)
+        long_end=long_lat(k , 3)
+        lat_end=long_lat(k , 4)
 
 
         %collect the delay 
-        delay_google=[];
         delay_temp=[];
-        %Collect the number of locations being queried
-        num_of_locations = [];
-
+        delay_google=[];
         for i = 1:length(num_of_steps)
                 for j = 1:num_of_query_per_location
                     fileId = fileId + 1 ;
@@ -44,29 +38,22 @@ for k=1:r
                     key_counter = key_counter + num_of_steps(i) ; % num_of_steps reperesents the  number of locations being queried in one json array (one request)
                     %disp(['key_counter' ,num2str(key_counter)]) % for debugging
                      cd([my_path,'/google']);
-                     
-                     class(num_of_steps(i))
-                     class(long_end)
-                      class(lat_start)
-                       class(lat_end)
-                       class(long_start)
 
                     [msg_google,delay_google_tmp,error_google_tmp]=...
-                        multi_location_query_google_interval(type,lat_start ,lat_end ,long_start,...
-                        long_end,num_of_steps(i),height,agl,key_counter, my_path );
+                        multi_location_query_google_interval(...
+                        lat_start ,lat_end ,long_start,long_end,num_of_steps(i),height , distance_divider ,key_counter, my_path );
 
                     delay_temp = [delay_temp  delay_google_tmp];
 
                     % writing the response to a file
                     if error_google_tmp==0
                         var_name_txt=strcat(num2str(fileId));
-                        var_name_zip=strcat(num2str(fileId))     
+                        var_name_zip=strcat(num2str(fileId));     
                         dlmwrite(['txt/',var_name_txt,'.txt'],msg_google,'');
                         zip(['zip/',var_name_zip], ['txt/',var_name_txt,'.txt']);
                         gzip( ['txt/',var_name_txt,'.txt'] ,'gzip/' );
                     end
                 end 
-
                 %Get the average of the delay of the same queried area
                 delay = sum(delay_temp)/length(delay_temp);
                 %collecting the averaged delay 
@@ -74,13 +61,15 @@ for k=1:r
                 delay_temp = [] ;
                 delay = [] ;
         end
-
     %%
         hold on
             plot(num_of_steps , delay_google , '-*', 'LineWidth' , 1);
             xlabel('Number of locations per one request');
             ylabel('Delay (sec)');  
+            delay_google_vec = [delay_google_vec delay_google];
+            delay_google = []; % reset required for the next step
 end
+legend('10km','10km','10km','50km','across US')
 hold off
 
 %%
--- multi_location_query_google_interval.m
+++ multi_location_query_google_interval.m
@@ -1,43 +1,29 @@
 function [response , delay , error] =  multi_location_query_google_interval...
-    (request_type, latitude_start, latitude_end, longitude_start,...
-    longitude_end ,num_of_steps,height,agl, key_counter, my_path)
+    ( latitude_start, latitude_end, longitude_start,...
+    longitude_end ,num_of_steps,height, distance_divider, key_counter, my_path)
 %%
+%Global Google parameters (refer to https://developers.google.com/spectrum/v1/paws/getSpectrum)
+agl='"AMSL"';
+request_type='"AVAIL_SPECTRUM_REQ"';
+server_name='https://www.googleapis.com/rpc';
+
 error=false; %Default error value
 delay=[]; %Default delay value
-
-server_name='https://www.googleapis.com/rpc';
 text_coding='"Content-Type: application/json ; charset=utf-8; "';
 device_type='"MODE_2"'; %Types of TVWS device: http://en.wikipedia.org/wiki/TV-band_device
 %%
- if key_counter < 1000
-         key='"AIzaSyAYMovM80dqfP8kwFXPyO8A-GMSOl-Bmu0"';%API selection
-    elseif key_counter < 2000
-         key='"AIzaSyAX0R4PSx9taUbFNyo9Ka5W1dGU2Xt72Dc"';
-     elseif key_counter < 3000
-         key='"AIzaSyCzAkKfQl93D8xAv_4kyMJSnvX1xz7VT6Q"';
-     elseif key_counter < 4000
-         key='"AIzaSyDESRe8rnVr4r-be1higwGmfAgE8nOS1CU"';
-     elseif key_counter < 5000
-          key='"AIzaSyBE-GOIVm2-uhWKeB1oINpmoSeyl7dUi3A"';
-     elseif key_counter < 6000
-          key='"AIzaSyCFBZevCyqYzwrW-i0mbb0sMtFSUt-rAnA"';
-     else
-         key='"AIzaSyBORRyv4vfrKJ6D73P0qJZjp-dgYlD9If4"';
- end
+key = key_selector(key_counter);
 
 %%
 query_generator_interval...
     (request_type,device_type,latitude_start,latitude_end ,...
-    longitude_start, longitude_end ,num_of_steps,height,agl,key, my_path)
+    longitude_start, longitude_end ,num_of_steps,height,distance_divider ,agl,key, my_path)
 
 %my_path=regexprep(my_path,' ','\\ ');
 
 cmnd=['/usr/bin/curl -X POST   ',server_name,' -H ',text_coding,' --data-binary @',my_path,'/google/google.json -w %{time_total}'];
 %cmnd=['/usr/bin/curl -X POST  ',server_name,' -v -i -H "accept-encoding: gzip" ',text_coding,' --data-binary @',my_path,'/google.json -w %{time_total}'];
 [status,response]=system(cmnd);
-disp(response)
-disp('status ----->')
-disp(status)
 warning_google='Daily Limit Exceeded'; %Error handling in case of exceeed API limit
 
 if ~isempty(findstr(response,warning_google));
@@ -61,16 +47,11 @@ end
 
 function  query_generator_interval...
     (request_type,device_type,latitude_start,latitude_end ,...
-    longitude_start, longitude_end ,num_of_steps,height,agl,key, my_path)
+    longitude_start, longitude_end ,num_of_steps,height,distance_divider,agl,key, my_path)
 %This function will generate the json array requests along a line between
 %two points
 
 % Dividing the distance into segmets to be queried gradually 
-distance_divider = 256 ;
-
- cd([my_path,'/google']);
-%To start the json array
-dlmwrite('google.json','[','delimiter','');
 
 longitude = linspace(longitude_start,longitude_end , distance_divider);
 latitude = linspace(latitude_start,latitude_end , distance_divider);
@@ -82,13 +63,11 @@ latitude = linspace(latitude_start,latitude_end , distance_divider);
     else
         comma = '';
     end
-    
-%     if num_of_steps == 1
-%         count = 1;
-%     else 
-%         count = num_of_steps - 1; % -1 is need it because initially it is set to 1 and then increased by 10
-%     end
-    
+
+ cd([my_path,'/google']);
+%To start the json array
+dlmwrite('google.json','[','delimiter','');
+
 for i = 1:num_of_steps 
     if i == num_of_steps
         comma='';
