3732adec923a175189d9795d377b6961f5022b41
przemyslaw-pawelczak <przemyslaw-pawelczak@users.noreply.github.com>
2015-01-07 22:10:52 +0100

    Update multi_location_query_google_interval.m
    Added copyright boilerplate

--- multi_location_query_google_interval.m
+++ multi_location_query_google_interval.m
@@ -1,16 +1,47 @@
-%   Reference: [1] "Will Dynamic Spectrum Access Drain my
-%   Battery?", submitted for publication, July 2014
-
-%   Code development: Amjed Yousef Majid (amjadyousefmajid@student.tudelft.nl), Przemyslaw Pawelczak (p.pawelczak@tudelft.nl)
+function [response, delay, error] =  multi_location_query_google_interval...
+    (latitude_start, latitude_end, longitude_start, ...
+    longitude_end, num_of_steps,height, distance_divider, key_counter, my_path)
+% MULTI_LOCATION_QUERY_GOOGLE_INTERVAL Query Google White Space (multiple locations).
 
 %   Last update: 7 January 2015
 
-%   This work is licensed under a Creative Commons Attribution 3.0 Unported
-%   License. Link to license: http://creativecommons.org/licenses/by/3.0/
+% Reference:
+%   P. Pawelczak et al. (2014), "Will Dynamic Spectrum Access Drain my
+%   Battery?," submitted for publication.
 
-function [response , delay , error] =  multi_location_query_google_interval...
-    (latitude_start, latitude_end, longitude_start,...
-    longitude_end ,num_of_steps,height, distance_divider, key_counter, my_path)
+%   Code development: Amjed Yousef Majid (amjadyousefmajid@student.tudelft.nl), 
+%                     Przemyslaw Pawelczak (p.pawelczak@tudelft.nl)
+
+% Copyright (c) 2014, Embedded Software Group, Delft University of 
+% Technology, The Netherlands. All rights reserved.
+% 
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions
+% are met:
+% 
+% 1. Redistributions of source code must retain the above copyright notice,
+% this list of conditions and the following disclaimer.
+% 
+% 2. Redistributions in binary form must reproduce the above copyright
+% notice, this list of conditions and the following disclaimer in the
+% documentation and/or other materials provided with the distribution.
+% 
+% 3. Neither the name of the copyright holder nor the names of its
+% contributors may be used to endorse or promote products derived from this
+% software without specific prior written permission.
+% 
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+% "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+% LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+% PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+% HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+% SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+% TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+% PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+% LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+% NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+% SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 %%
 %Global Google parameters (refer to https://developers.google.com/spectrum/v1/paws/getSpectrum)
 agl = '"AMSL"';
